8274da054756a2544b0912bc2f4ef0b54a840b13
SheaMeyers <shea.meyers@gmail.com>
2019-05-26 11:38:41 +0200

    Import npm model and use npm init

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-const getNotes = require('./notes.js');
-const result = getNotes('');
-console.log(result);
+const validator = require('validator')
+const getNotes = require('./notes.js')
+
+const msg = getNotes()
+console.log(msg)
+
+console.log(validator.isURL('https://mead.io'))
